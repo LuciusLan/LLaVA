5f1eb498dba388f26bcc88fb0c4fa11e02dfcbde
luciuslan <cifellan@hotmail.com>
2024-05-23 05:39:19 +0000

    merge language prior inference

--- llava/language_prior_w_img.py
+++ llava/language_prior_w_img.py
@@ -1,7 +1,7 @@
 import argparse
 import math
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+#os.environ['CUDA_VISIBLE_DEVICES'] = ''
 os.environ['HF_HOME'] = '/home/wuyin/hf_cache/'
 import sys
 sys.path.insert(1, os.getcwd())
@@ -14,8 +14,9 @@ from tqdm import tqdm
 #import shortuuid
 import torch
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 import numpy as np
+from PIL import Image
 
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.conversation import conv_templates, SeparatorStyle
@@ -24,7 +25,16 @@ from llava.utils import disable_torch_init
 from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
 
 
+class CustomDataset(Dataset):
+    def __init__(self, questions) -> None:
+        super().__init__()
+        self.questions = questions
+    
+    def __len__(self):
+        return len(self.questions)
 
+    def __getitem__(self, index) -> Any:
+        return self.questions[index]['question']['question'], self.questions[index]['question']['image_id']
 
 def eval_model(args):
     # Model
@@ -34,7 +44,34 @@ def eval_model(args):
     #CQ: change for attention map, need eager not sdpa
     # tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name) # CQ: add for attention map
+    #model = model.to(torch.float32)
+    def custom_collate(batch):
+        question_texts = [e[0] for e in batch]
+        image_ids = [e[1] for e in batch]
+        image_ids = [f"COCO_val2014_{'0' * (12 - len(str(e))) + str(e)}.jpg" for e in image_ids]
+        images = [Image.open(os.path.join(args.image_folder, image_file)).convert('RGB') for image_file in image_ids]
+        image_sizes = [i.size for i in images]
+        images = process_images(images, image_processor, model.config)
+
+        prompts = []
+        for qs in question_texts:
+            qs = "Please answer the question. Give your answer with the answer keyword(s) only, make it as short as possible.\n" + qs +"\nAnswer:"
+
+            if model.config.mm_use_im_start_end:
+                qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+
+            conv = conv_templates[args.conv_mode].copy()
+            conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
+            prompts.append(conv.get_prompt())
+
+        input_ids = [tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX) for prompt in prompts]
+        input_ids = torch.nn.utils.rnn.pad_sequence([torch.LongTensor(e[::-1]) for e in input_ids], batch_first=True, padding_value=tokenizer.eos_token_id).flip(dims=[1])
+        return input_ids, images, image_sizes
         
+
     # MODEL:
     #print(model)
     with open('./vqa_color.jsonl') as f:
@@ -42,25 +79,36 @@ def eval_model(args):
         questions = [json.loads(e) for e in questions]
     f.close()
 
-    #question_texts = [f"[INST]Please answer the question. Give your answer with the answer keyword(s) only, make it as short as possible.\n {e['question']['question']}\nAnswer:[/INST]" for e in questions]
-    question_texts = [f"[INST]{e['question']['question']}[/INST]" for e in questions]
-    question_ids = tokenizer(question_texts, padding='longest', return_tensors='pt')
-    index = torch.LongTensor(np.arange(len(questions))).unsqueeze(1)
-
-
-    ds = TensorDataset(question_ids.input_ids, question_ids.attention_mask, index)
-    dl = DataLoader(ds, batch_size=8)
     output_probs = []
-    for batch in tqdm(dl, total=(len(dl))):
-        input_ids, attention_mask, index = batch
+
+    ds = CustomDataset(questions)
+    dl = DataLoader(ds, batch_size=8, collate_fn=custom_collate)
+
+    for batch in tqdm(dl, total=len(dl)):
+        #qs = question['question']['question']
+        #qs = "Please answer the question. Give your answer with the answer keyword(s) only, make it as short as possible.\n" + qs
+        #img_id = '0' * (12 - len(str(question['question']['image_id']))) + str(question['question']['image_id'])
+        #image_file = f"COCO_val2014_{img_id}"
+
+        #conv = conv_templates[args.conv_mode].copy()
+        #conv.append_message(conv.roles[0], qs)
+        #conv.append_message(conv.roles[1], None)
+        #prompt = conv.get_prompt()
+
+        #input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
+        #image = Image.open(os.path.join(args.image_folder, image_file)).convert('RGB')
+        #image_tensor = process_images([image], image_processor, model.config)[0]
+
+        input_ids, images, image_sizes = batch
         input_ids = input_ids.cuda()
-        attention_mask = attention_mask.cuda()
+
+
         with torch.inference_mode():
             output_ids = model.generate(
                 input_ids,
-                attention_mask=attention_mask,
-                images=None,
-                image_sizes=None,
+                images=images.half().cuda(),
+                image_sizes=image_sizes,
                 do_sample=True if args.temperature > 0 else False,
                 temperature=args.temperature,
                 top_p=args.top_p,
@@ -86,9 +134,9 @@ def eval_model(args):
 
             output_probs.append([{'token': tokenizer.decode(tok), 'probs':scores.cpu().tolist(), 'top10_tokens': top10_id.cpu().tolist()} for tok, scores, top10_id in zip(output_ids.sequences[i], top10_prob, tt)])
 
-    with open('output_scores.jsonl', 'w') as f:
+    with open('output_scores_w_img.jsonl', 'w') as f:
         for row in output_probs:
-            f.write(json.dumps(row, f))
+            f.write(json.dumps(row))
             f.write('\n')
     f.close()
 
